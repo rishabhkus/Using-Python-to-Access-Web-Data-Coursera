0c53d99b039d8a6f673837c3b459f2105655d4fb
rishabhkus <kushawahrishabh@gmail.com>
2018-12-10 18:11:16 +0530

    Third commit

---
+++ Assignment 1.py
@@ -0,0 +1,12 @@
+import re
+fname = raw_input('Enter File name :')
+handle = open(fname)
+sum=0
+count = 0
+for line in handle:
+	f = re.findall('[0-9]+',line)
+	for num in f:
+		if num >= [0]:
+			count = count + 1
+			sum = sum + int(num)
+print 'There are',count,'values with a sum =',sum
